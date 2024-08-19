f7a5f394e6e6ae77d03c1d90a480eb150bbe6402
Blitzbomber87 <elias.efly000@gmail.com>
2024-08-19 11:51:27 -0700

    Added server.js functionality and readme

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Note-Taker
+
+## Description
+
 Module-11-Challenge-Express.js
+
+Keep your thoughts organized with this note taker.  Write and save notes on the go.
+
+User Story
+AS A small business owner
+I WANT to be able to write and save notes
+SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+
+## Installation
+
+1. Clone the repository: https://github.com/blitzbomber87/Note-Taker
+2. Run command "npm install express" and "npm install uuid"
+
+## Usage
+
+Open Render link here...
+
+Homepage to start note taking
+
+![hoepage]
+
+T
+
+![readme]
+
+
+
+
+## Credits
+Eli - https://github.com/blitzbomber87
+
+
+
+
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid'); // To generate unique IDs
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON and URL-encoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = { id: uuidv4(), ...req.body };
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    notes.push(newNote);
+    fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
+      if (err) throw err;
+      res.json(newNote);
+    });
+  });
+});
+
+// Bonus: DELETE route
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const filteredNotes = notes.filter(note => note.id !== noteId);
+    fs.writeFile('./db/db.json', JSON.stringify(filteredNotes, null, 2), (err) => {
+      if (err) throw err;
+      res.json({ success: true });
+    });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
